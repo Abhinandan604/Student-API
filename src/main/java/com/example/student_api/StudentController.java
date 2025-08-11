8fe3036c6c9f82df41fcf99a37618969e978be54
Abhinandan604 <abhinandanraj604@gmail.com>
2025-08-11 20:34:41 +0530

    changes in controller

--- src/main/java/com/example/student_api/StudentController.java
+++ src/main/java/com/example/student_api/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
+@CrossOrigin(origins = "http://localhost:5500") // Allow frontend
 public class StudentController {
 
     @Autowired
